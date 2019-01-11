8610a7368c76b62a73c4a277d9451956080a7fc5
Mitchel Parish <mitchelparish@gmail.com>
2019-01-11 16:27:38 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -12,40 +12,53 @@ def dictionary
    "and" => "&"
 }
 end
+def dictionary
+  convert = {
+   "hello" => "hi",
+   "to" => "2",
+   "two" => "2",
+   "too" => "2",
+   "for" => "4",
+   "four" => "4",
+   "be" => "b",
+   "you" => "u",
+   "at" => "@",
+   "and" => "&"
+}
+end
 
 def word_substituter(tweet)
-  dict = dictionary
-  words = tweet.split(" ")
+  tweet.split(" ").map do |word|
+    if dictionary.keys.include?(word.downcase)
+        word = dictionary[word.downcase]
+    else
+      word
+    end
+  end.join(" ")
+end
 
-  words.collect do |word|
-    dictionary.collect do |key, value|
-      if word == key
-        word.replace(value)
-      end
+def bulk_tweet_shortener(tweet)
+  tweet.map do |phrase|
+    puts word_substituter(phrase)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  tweet.split(" ").map do |phrase|
+    if phrase.length > 140
+      word_substituter(phrase)
+    elsif phrase.length <= 140
+      phrase
     end
   end
-  words.join(" ")
 end
-
-
-def bulk_tweet_shortener(tweet)
-  tweet.map do |phrase|
-    puts word_substituter(phrase)
-  end
-end
-
-def bulk_tweet_shortener(tweet)
-  tweet.map do |phrase|
-    puts word_substituter(phrase)
-  end
-end
-
-
 
 def shortened_tweet_truncator(tweet)
-  if word_substituter(tweet).chars.length > 140
-    truncated = word_substituter(tweet).chars[0..136].push("...").join("")
-  else
-    word_substituter(tweet)
-  end
+  tweet.split(" ").map do |phrase|
+    if phrase.length > 140
+      word_substituter(phrase)[0..140] + "..."
+    else
+      phrase
+    end
+  end.join(" ")
 end
