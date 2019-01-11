1656ff858d71ebb853aef2b4b04a10141a6fc55e
Mitchel Parish <mitchelparish@gmail.com>
2019-01-11 16:28:14 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -44,14 +44,12 @@ def bulk_tweet_shortener(tweet)
 end
 
 def selective_tweet_shortener(tweet)
-  tweet.split(" ").map do |phrase|
-    if phrase.length > 140
-      word_substituter(phrase)
-    elsif phrase.length <= 140
-      phrase
-    end
-  end
-end
+  if tweet.chars.length > 140  
+    word_substituter(tweet)
+  else 
+    tweet 
+  end 
+end 
 
 def shortened_tweet_truncator(tweet)
   tweet.split(" ").map do |phrase|
