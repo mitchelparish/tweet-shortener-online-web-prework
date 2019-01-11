d1ccbcf120b72202e98a671d3f010fef4fea1def
Mitchel Parish <mitchelparish@gmail.com>
2019-01-11 16:22:42 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -35,7 +35,7 @@ def selective_tweet_shortener(tweet)
   tweet.split(" ").map do |phrase|
     if phrase.length > 140
       word_substituter(phrase)
-    elsif phrase.length <= 140
+    elsif phrase.length < 140
       phrase
     end
   end
