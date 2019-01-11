ac581faed054160228f136cdff446ef2ef773368
Mitchel Parish <mitchelparish@gmail.com>
2019-01-11 16:28:51 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -12,20 +12,6 @@ def dictionary
    "and" => "&"
 }
 end
-def dictionary
-  convert = {
-   "hello" => "hi",
-   "to" => "2",
-   "two" => "2",
-   "too" => "2",
-   "for" => "4",
-   "four" => "4",
-   "be" => "b",
-   "you" => "u",
-   "at" => "@",
-   "and" => "&"
-}
-end
 
 def word_substituter(tweet)
   tweet.split(" ").map do |word|
