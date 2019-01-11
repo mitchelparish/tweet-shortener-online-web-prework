502c61ca4da90ef14f7d6492f1285a2912530ac0
Mitchel Parish <mitchelparish@gmail.com>
2019-01-11 16:21:59 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,29 +1,37 @@
 def dictionary
   convert = {
-    "hello" => "hi",
-    "to" => "2",
-    "two" => "2",
-    "too" => "2",
-    "for" => "4",
-    "four" => "4",
-    "be" => "b",
-    "you" => "u",
-    "at" => "@",
-    "and" => "&"
-  }
+   "hello" => "hi",
+   "to" => "2",
+   "two" => "2",
+   "too" => "2",
+   "for" => "4",
+   "four" => "4",
+   "be" => "b",
+   "you" => "u",
+   "at" => "@",
+   "and" => "&"
+}
 end
 
 def word_substituter(tweet)
-  tweet.split(" ").map do |word|
-    if dictionary.keys.include?(word.downcase)
-      word = dictionary[word.downcase]
-    else
-      word
+  # replaces long words with their expected short form
+
+    tweet.split(" ").map do |word|
+      if dictionary.keys.include?(word.downcase)
+        word = dictionary[word.downcase]
+      else
+        word
     end
   end.join(" ")
 end
 
 def bulk_tweet_shortener(tweet)
+  tweet.map do |phrase|
+    puts word_substituter(phrase)
+  end
+end
+
+def selective_tweet_shortener(tweet)
   tweet.split(" ").map do |phrase|
     if phrase.length > 140
       word_substituter(phrase)
